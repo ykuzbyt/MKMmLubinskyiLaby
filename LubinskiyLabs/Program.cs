74c7a710b72fa9275787a72ed2ac31e2cc381d8a
Yuriy Kuzbyt <ykuzbyt@Yuriys-MacBook-Pro.local>
2016-03-25 14:00:47 +0200

    Create project

---
+++ LubinskiyLabs/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LubinskiyLabs
+{
+    class MainClass
+    {
+        public static void Main (string[] args)
+        {
+            Console.WriteLine ("Hello World!");
+        }
+    }
+}
