92a1b2440ead4089d74ffa1c06fd4c5c76b2648a
nvlizlo <111111s>
2016-05-17 10:46:10 +0300

    Add realization for class Car

--- calc/GitDemo/Car.cs
+++ calc/GitDemo/Car.cs
@@ -4,9 +4,23 @@ namespace GitDemo
 {
     public class Car
     {
-        public Car ()
+        public Car(string color)
         {
+            Color = color;
+        }
+
+        public string Color { get; set; }
+
+        private int PassangerCount { get; private set; }
+
+        public void StartEngine()
+        {
+            Console.WriteLine("Engine started!!!");
+        }
+
+        public void AddPassanger()
+        {
+            ++PassangerCount;
         }
     }
 }
-
