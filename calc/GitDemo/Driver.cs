3dc200f05aa37ebaaf792cf1db55485062e86163
Yuriy Kuzbyt <ykuzbyt@Yuriys-MacBook-Pro.local>
2016-05-17 10:50:09 +0300

    Added realization for Class Driver

--- calc/GitDemo/Driver.cs
+++ calc/GitDemo/Driver.cs
@@ -4,9 +4,19 @@ namespace GitDemo
 {
     public class Driver
     {
-        public Driver ()
+        public Driver (string name, int age)
         {
+            Name = name;
+            Age = age;
+        }
+
+        public string Name { get; private set; }
+        public int Age { get; private set; }
+        private Car Car { get; private set; }
+        public void BuyCar(Car a)
+        {
+            Car = a;
+            a.AddPassanger(this);
         }
     }
 }
-
